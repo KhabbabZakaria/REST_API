8818a91c159eb25f9830fb7666b2410df5d77181
Zakar <zakariak.engg@gmail.com>
2022-06-01 14:10:09 +0200

    api using of stack exchange

---
+++ api_stackexchange.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+#print(response.json())
+
+for question in response.json()['items']:
+    print(question['title'])
