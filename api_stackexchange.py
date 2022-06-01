d4a8987d0dd17acbe24c9a880273da39213d9f9e
Zakar <zakariak.engg@gmail.com>
2022-06-01 20:43:03 +0200

    check all links with low answer counts

--- api_stackexchange.py
+++ api_stackexchange.py
@@ -2,8 +2,16 @@ import requests
 import json
 
 response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+'''keysList = list(response.json()['items'][0].keys())
+print(keysList)
 
-#print(response.json())
+print(response.json()['items'][0])'''
+
+'''for question in response.json()['items']:
+    print(question['title'])'''
 
 for question in response.json()['items']:
-    print(question['title'])
+    if question['answer_count']<2:
+        print(question['title'])
+        print(question['link'])
+        print()
